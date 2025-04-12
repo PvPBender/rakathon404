16f4511a3cbca08e8828391a317ad3a8f523ce8e
Barbora Kindlova <kindlova.barbora12@gmail.com>
2025-04-12 22:08:25 +0200

    Patology

--- loader/handlePat.py
+++ loader/handlePat.py
@@ -1,8 +1,15 @@
 import pandas as pd
-from model.patientTemplate import PacientTemplate
+
+from db.tables import Patolog
+from model.patientTemplate import PacientTemplate, M_1_1
 import logging
 
-def handlePat(pat_entry, patient: PacientTemplate):
+
+def handlePat(pat_entry: list[Patolog], patient: PacientTemplate):
     logging.info(f"Handling PAT: {pat_entry}")
+    print("------POHLAVI: " + pat_entry.pohlavi)
+    print("------ROK: " + str(pat_entry.rok))
+
+    patient.M_1.M_1_1.M_1_1_6 = pat_entry.pohlavi
 
     return patient
