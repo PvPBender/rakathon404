b1518fed41f777151bc340c13a814fe86e2d8232
Dalibor Trampota <dalibor.trampota@gmail.com>
2025-04-12 09:29:35 +0200

    Database setup

---
+++ db/tables/Base.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import DeclarativeBase
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Enum
+import enum
+
+class Base(DeclarativeBase):
+    pass
+
+
+# class Gender(enum.Enum):
+#     male = 0
+#     female = 1
+#     other = 2
+
+
+# class Pacient(Base):
+#     __tablename__ = "pacient"
+
+#     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
+#     year: Mapped[int]
+#     gender: Mapped[Gender] = mapped_column(Enum(Gender))
+
+
+#     def __repr__(self) -> str:
+#         return f"Pacient(ID={self.id!r}, year={self.year!r}, sex={self.gender!r})"
